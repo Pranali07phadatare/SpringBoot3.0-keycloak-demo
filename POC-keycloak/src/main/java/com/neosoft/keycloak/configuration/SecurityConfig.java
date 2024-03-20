92620da7734e9d2637d3425b31499ed3affe5c76
Pranali07phadatare <100848878+Pranali07phadatare@users.noreply.github.com>
2024-03-20 12:09:12 +0530

    Add files via upload

---
+++ POC-keycloak/src/main/java/com/neosoft/keycloak/configuration/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.neosoft.keycloak.configuration;
+
+import com.neosoft.keycloak.helper.JwtAuthConverter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtAuthConverter jwtAuthConverter;
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthConverter))
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(STATELESS)
+                )
+                .build();
+    }
+}
